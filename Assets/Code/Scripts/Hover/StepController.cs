2af866b40663a2d532ff4d10fdcac9abe338ab06
FlavioNovati <f.novati10@gmail.com>
2024-01-17 10:43:21 +0100

    Fixed Step Controller

--- Assets/Code/Scripts/Hover/StepController.cs
+++ Assets/Code/Scripts/Hover/StepController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SocialPlatforms;
 
@@ -18,11 +19,14 @@ public class StepController : MonoBehaviour
     [SerializeField] private float StepDetectorLenght = 0.5f;
     [Tooltip("Min Legth of the detector")]
     [SerializeField] private float StepDetectorStartLenght = 0.2f;
-
+    [Tooltip("Aperture of the rays")]
+    [SerializeField] Vector3 StepRayAperture = new Vector3(0.5f, 0.0f, 0.0f);
     [Tooltip("Layer that collide with Raycasts")]
     [SerializeField] LayerMask StepLayerMask;
-    [Tooltip("Color of the detector")]
+    [Tooltip("Color of the preview")]
     [SerializeField] private Color StepDetectorColor = Color.yellow;
+    [Tooltip("Color of the rays")]
+    [SerializeField] private Color RayColor = Color.magenta;
     #endregion
 
     private Rigidbody Body;
@@ -34,43 +38,61 @@ public class StepController : MonoBehaviour
 
     public void CheckStep(float linearSpeed)
     {
+        if (Body.velocity == Vector3.zero)
+            return;
+
         bool step = false;
         float rayLenght = Mathf.Clamp(StepDetectorLenght * linearSpeed, StepDetectorStartLenght, StepDetectorLenght);
         Vector3 rayDir = new Vector3(Body.velocity.normalized.x, 0.0f, Body.velocity.normalized.z);
         //Create rays var
-        Ray lowerRay = new Ray(new Vector3(transform.position.x, transform.position.y + StartY, transform.position.z), rayDir);
-        Ray upperRay = new Ray(new Vector3(transform.position.x, transform.position.y + StartY + StepHeight, transform.position.z), rayDir);
+        Ray[] rays = new Ray[4];
+        //Lower Rays
+        rays[0] = new Ray(new Vector3(transform.position.x, transform.position.y + StartY, transform.position.z), rayDir + StepRayAperture);
+        rays[1] = new Ray(new Vector3(transform.position.x, transform.position.y + StartY, transform.position.z), rayDir - StepRayAperture);
+        //Upper Rays
+        rays[2] = new Ray(new Vector3(transform.position.x, transform.position.y + StartY + StepHeight, transform.position.z), rayDir + StepRayAperture);
+        rays[3] = new Ray(new Vector3(transform.position.x, transform.position.y + StartY + StepHeight, transform.position.z), rayDir - StepRayAperture);
 
-        if (Physics.Raycast(lowerRay, out RaycastHit lowerHit, rayLenght, StepLayerMask))
-            step = true;
-        if (Physics.Raycast(upperRay, out RaycastHit upperHit, rayLenght, StepLayerMask))
-            step = false;
+        step = IsAStep(rays, rayLenght);
 
         if (step)
-        {
             transform.position = new Vector3(transform.position.x, transform.position.y + StepHeight, transform.position.z);
-        }
 
         //DEBUG Ray
-        Debug.DrawRay(lowerRay.origin, lowerRay.direction * rayLenght, Color.red, Time.deltaTime);
-        Debug.DrawRay(upperRay.origin, upperRay.direction * rayLenght, Color.red, Time.deltaTime);
+        for(int i = 0; i < rays.Length; i++)
+            Debug.DrawRay(rays[i].origin, rays[i].direction * rayLenght, RayColor, Time.deltaTime);
+    }
 
+    //Rays lenght is divided by 2
+    private bool IsAStep(Ray[] rays, float rayLenght)
+    {
+        for (int i = 0; i < rays.Length / 2; i++)
+            if (Physics.Raycast(rays[i], out RaycastHit hit, rayLenght, StepLayerMask))
+                return true;
+
+        return false;
     }
 
     private void OnDrawGizmos()
     {
-        #region Draw Step Detection
+        #if UNITY_EDITOR
+        #region Draw Step Detection preview
         Gizmos.color = StepDetectorColor;
-
-        //Ray direction
-        Vector3 direction = Vector3.forward * StepDetectorLenght;
-        //All ray start in 0.0f xf 0.0f locally
-        Vector3 rayOrigin = new Vector3(transform.position.x, transform.position.y + StartY, transform.position.z);
-        //Draw Lower Ray
-        Gizmos.DrawRay(rayOrigin, direction);
-        //Draw Upper Ray
-        rayOrigin.y += StepHeight;
-        Gizmos.DrawRay(rayOrigin, direction);
+        if (!EditorApplication.isPlaying)
+        {
+            //Ray direction
+            Vector3 direction = Vector3.forward * StepDetectorLenght;
+            //All ray start in 0.0f xf 0.0f locally
+            Vector3 rayOrigin = new Vector3(transform.position.x, transform.position.y + StartY, transform.position.z);
+            //Draw Lower Rays
+            Gizmos.DrawRay(rayOrigin, direction + StepRayAperture);
+            Gizmos.DrawRay(rayOrigin, direction - StepRayAperture);
+            //Draw Upper Ray
+            rayOrigin.y += StepHeight;
+            Gizmos.DrawRay(rayOrigin, direction + StepRayAperture);
+            Gizmos.DrawRay(rayOrigin, direction - StepRayAperture);
+        }
         #endregion
+        #endif
     }
 }
